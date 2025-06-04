eb303094fd3378197e89aa28c1cf441d8d628744
Miguel Ángel de la Rosa Leva <miguelangeldelarosaleva@gmail.com>
2025-06-04 20:47:59 +0200

    Modificación de estilos

---
+++ gestion-bares-frontend/src/features/Privacy.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "../layouts/Footer"
+import { Header } from "../layouts/Header"
+
+export const Privacy = () => {
+  return <>
+    <div className="min-h-screen flex flex-col bg-slate-50">
+      <Header />
+      <main className="container mx-auto flex-1 flex flex-col gap-6 text-slate-700 rounded-lg p-8">
+      </main>
+      <Footer />
+    </div>
+  </>
+}
