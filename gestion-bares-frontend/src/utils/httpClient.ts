4577ce63422c63392e43493874aae2d7f19940aa
Miguel Ángel de la Rosa Leva <miguelangeldelarosaleva@gmail.com>
2025-06-11 21:33:23 +0200

    actualizacion de rutas en el front para despliegue

--- gestion-bares-frontend/src/utils/httpClient.ts
+++ gestion-bares-frontend/src/utils/httpClient.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import queryString from "query-string";
 
 const httpClient = axios.create({
-  baseURL: "http://localhost:8080/api",
-  // baseURL: "http://ruizgijon.ddns.net:8080",
+  // baseURL: "http://localhost:8080/api",
+  baseURL: "http://ruizgijon.ddns.net:8080",
   paramsSerializer: (params) =>
     queryString.stringify(params, { arrayFormat: "comma" }),
   headers: {
