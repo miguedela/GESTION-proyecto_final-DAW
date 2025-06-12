031af512c6d5c143e25753a2748d975c77d9373b
Miguel Ángel de la Rosa Leva <miguelangeldelarosaleva@gmail.com>
2025-06-12 18:46:53 +0200

    Header y footer responsive

--- gestion-bares-frontend/src/layouts/Header.tsx
+++ gestion-bares-frontend/src/layouts/Header.tsx
@@ -9,20 +9,20 @@ export const Header = () => {
 
     return <>
         <header className="w-full sm:w-[95%] sm:top-4 left-1/2 -translate-x-1/2 fixed z-50 mx-auto sm:rounded-2xl bg-slate-50/80 border-b border-slate-200 shadow-sm text-slate-900 backdrop-blur-xs">
-            <div className="mx-auto flex items-center justify-between gap-2 py-4 px-6 md:px-12">
+            <div className="mx-auto flex flex-wrap items-center justify-between gap-2 py-4 px-6 md:px-12">
                 <Link to="/main">
                     <div className="flex items-end gap-2 transition-transform hover:scale-110 active:scale-95">
                         <img className="min-w-10 w-25" src="/img/logopng.png" alt="logo" />
                     </div>
                 </Link>
-                <nav className="flex gap-6 items-center">
+                <nav className="flex flex-wrap gap-6 items-center">
                     <Link to="/help" className="flex items-center gap-1 text-slate-700 hover:text-amber-500 transition-colors duration-200">
                         <IoInformationCircleOutline size={30} className="transition-transform hover:scale-125 active:scale-95" />
                     </Link>
                     <Link to="/contact" className="flex items-center gap-1 text-slate-700 hover:text-amber-500 transition-colors duration-200">
                         <IoCallOutline size={26} className="transition-transform hover:scale-125 active:scale-95" />
                     </Link>
-                    <div className="h-6 w-px bg-slate-900" />
+                    <div className="h-6 w-px bg-slate-900 hidden sm:block" />
                     {user?.role !== Roles.ADMIN || !user && (
                         <Link to={user?.role === Roles.CUSTOMER || user?.role === Roles.STAFF ? "/notifications" : "/my-notifications"} className="flex items-center gap-1 text-slate-700 hover:text-amber-500 transition-colors duration-200">
                             <IoNotificationsOutline size={26} className="transition-transform hover:scale-125 active:scale-95" />
