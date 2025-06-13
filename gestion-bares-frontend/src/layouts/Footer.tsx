e5c485f86b5f8dbacc46027c0171777bc7608837
Miguel Ángel de la Rosa Leva <miguelangeldelarosaleva@gmail.com>
2025-06-13 08:17:31 +0200

    footer

--- gestion-bares-frontend/src/layouts/Footer.tsx
+++ gestion-bares-frontend/src/layouts/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => (
             <span className="font-semibold tracking-wide flex items-center gap-2">
                 ©{new Date().getFullYear()}<span className="font-bold text-amber-600">TapaTech</span>Derechos reservados.
             </span>
-            <div className="flex flex-wrap gap-4 mt-2 md:mt-0">
+            <div className="flex flex-wrap gap-4">
                 <a href="/privacy" className="text-slate-100 hover:text-amber-500 transition-colors">Privacidad</a>
                 <a href="/terms" className="text-slate-100 hover:text-amber-500 transition-colors">Términos</a>
             </div>
